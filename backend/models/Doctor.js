f26ab1717a80146106a02a0b43458748ccff03f1
MohsinKhan <khanmdmohsin1802@gmail.com>
2026-04-21 19:18:33 +0530

    create : folders | add : models | package add : jwt,dotenv

---
+++ backend/models/Doctor.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import User from "./User.js";
+
+//Doctors can only select from this --> this is to avoid typos and junk inputs
+const validSpecialty = [
+  "General Physician",
+  "Cardiologist",
+  "Dermatologist",
+  "Pediatrician",
+  "Orthopedic",
+];
+
+const doctorSchema = new mongoose.Schema({
+// doctors specialty attribute
+  specialty: {
+    type: String,
+    required: true,
+    enum : {
+        values : validSpecialty,
+    }
+  },
+// doctors years of experience attribute
+  experienceYears: {
+    type: Number,
+  },
+// wheater doctor is accepting or not attribute
+  isAcceptingPatients: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Doctor = User.discriminator("doctor", doctorSchema);
+
+export default Doctor;
---
+++ backend/models/Patient.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import User from "./User.js";
+const patientSchema = new mongoose.Schema({
+    age : {
+        type : Number,
+    },
+    weight : {
+        type : Number
+    }
+})
+
+const Patient = User.discriminator('patient', patientSchema);
+
+export default Patient;
